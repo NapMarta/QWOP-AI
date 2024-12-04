532929f0a275cd7c3bb3334dd755089f16fb395d
francesco-giorgione <francesco.giorgione01@gmail.com>
2024-12-04 16:30:00 +0100

    Aggiunte funzioni di testing

--- training/SARSA_training.py
+++ training/SARSA_training.py
@@ -3,21 +3,30 @@ from .utils import *
 
 
 def train(num_episodes, env, agent):
+    worker(num_episodes, env, agent, training=True)
+
+
+def test(num_episodes, env, agent):
+    worker(num_episodes, env, agent, training=False)
+
+
+def worker(num_episodes, env, agent, training):
     for i in range(num_episodes):
         episode_reward = 0
         curr_state = agent.export_state(agent.env.reset()[0])
-        curr_action = agent.get_action(curr_state)
+        curr_action = agent.get_action(curr_state, training)
 
         while True:
             next_state, reward, terminated, truncated, info = env.step(curr_action)
             next_state = agent.export_state(next_state)
-            next_action = agent.get_action(next_state)
+            next_action = agent.get_action(next_state, training)
 
             # Reward personalizzata
             episode_reward += reward + info['distance']/100
             # print(episode_reward)
 
-            agent.update_qval(curr_state, curr_action, reward, terminated, next_state, next_action)
+            if training:
+                agent.update_qval(curr_state, curr_action, reward, terminated, next_state, next_action)
 
             if terminated or truncated:
                 break
@@ -31,6 +40,7 @@ def main(gamma=0.1, alpha=0.1, eps=0.2):
     env = get_init_env()
     agent = SARSAAgent(env, gamma, alpha, eps)
     train(10, env, agent)
+    test(1, env, agent)
     env.close()
 
 
