b4ea3f76fbdb1624f778a2fcf89ea8a3c8cb54b1
NapMarta <marta.napolillo@outlook.it>
2024-12-06 16:26:33 +0100

    Aggiunta file con path di configurazione

--- training/utils.py
+++ training/utils.py
@@ -3,13 +3,37 @@ import qwop_gym
 import gymnasium as gym
 
 def get_init_env():
+
+    browser_path, driver_path = _get_path()
+
     env = gym.make(
         "QWOP-v1",
-        browser="C:/Program Files/Google/Chrome/Application/chrome.exe",
-        driver="C:/Users/fgfoo/OneDrive/Documents/Uni/magistrale/anno-2/IA/chromedriver-win64/chromedriver-win64/chromedriver.exe",
-        auto_draw=True,
-        stat_in_browser=True,
-        reduced_action_set=True
+        browser = browser_path,
+        driver = driver_path,
+        auto_draw = True,
+        stat_in_browser = True,
+        reduced_action_set = True
     )
 
     return env
+
+def _get_path():
+    file_path = "config_env.txt"
+    browser_path = ""
+    driver_path = ""
+
+    try:
+        with open(file_path, 'r') as file:
+            righe = file.readlines()
+            browser_path = righe[1].strip()  
+            driver_path = righe[3].strip()  
+
+    except FileNotFoundError:
+        print(f"Il file '{file_path}' non è stato trovato.")
+    except Exception as e:
+        print(f"Si è verificato un errore: {e}")
+
+    print("Browser Path:", browser_path)
+    print("Driver Path:", driver_path)
+
+    return browser_path, driver_path
