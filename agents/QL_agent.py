17a7000469160bab39890321836af3a09ff178db
francesco-giorgione <francesco.giorgione01@gmail.com>
2024-12-13 23:07:12 +0100

    Fixata e rinominata funzione td_update

--- agents/QL_agent.py
+++ agents/QL_agent.py
@@ -3,30 +3,20 @@ from agents.agent import Agent
 
 class QLAgent(Agent):
     def __init__(self, env, gamma, alpha, eps):
-        super().__init__(env, gamma, alpha, eps)
+        super().__init__(env, gamma, eps)
+        self.alpha = alpha
 
 
-    # Funzione che effettua l'aggiornamento della 
-    # action-value function nel Q-Learning
-    def td_update(self, curr_state, action, reward, next_state):
-        
-        # actions = set(action for (_, action) in self.q_values.keys())
-        
-        # if not actions:
-        #     max_q_next = 0
-        # else:
-        #     # Prende il massimo tra tutte le possibili azioni eseguibili dallo stato s'
-        #     max_q_next = max([self.q_values.get((next_state, action), 0) for action in actions])
-        #     print(max_q_next.value())
-
-        next_action = self.get_action(next_state, True)
-
-        if not next_action:
-            max_q_next =  0
-        else:   
+    # Funzione che effettua l'aggiornamento della action-value function nel Q-Learning
+    # unused_next_action è inutilizzato: serve a realizzare il polimorfismo
+    def update_qval(self, curr_state, action, reward, terminal, next_state, unused_next_action):
+        if terminal:
+            td_target = reward
+        else:
+            next_action = self.get_action(next_state, False)
             max_q_next = self.get_qval((next_state, next_action))
-        
-        td_target = reward + self.gamma * max_q_next
+            td_target = reward + self.gamma * max_q_next
+
         td_error = td_target - self.get_qval((curr_state, action))
         self.q_values[(curr_state, action)] += self.alpha * td_error
 
